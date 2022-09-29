aee09b229aa44c18848e30aeb51e01fa97a8033d
zDR9YHt5ooXU <83929232+zDR9YHt5ooXU@users.noreply.github.com>
2022-09-29 14:15:16 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,37 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { of, map, Observable, defer, switchMap, shareReplay } from 'rxjs';
 
 of('World')
   .pipe(map((name) => `Hello, ${name}!`))
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+
+interface Dataset {
+  score: number;
+}
+
+const Scores: Record<string, number> = {
+  team1: 100,
+  team2: 60,
+};
+
+const getObservable = (team: Observable<string>): Observable<Dataset> => {
+  const obsCache: Record<string, Observable<Dataset>> = {};
+  return team.pipe(
+    switchMap((teamName) => {
+      let cache = obsCache[teamName];
+      if (cache) {
+        return cache;
+      }
+      cache = obsCache[teamName] = defer(() => of(Scores[teamName])).pipe(
+        map((score) => {
+          return { score };
+        }),
+        shareReplay({ bufferSize: 1, refCount: true })
+      );
+      return cache;
+    })
+  );
+};
